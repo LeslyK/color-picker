6ec8102248cf10ef51d5bb5a7435fe2d48d5109f
LeslyK <kobadobo@gmail.com>
2017-03-03 11:24:02 +0100

    ajout gradient

--- js/app.js
+++ js/app.js
@@ -1,23 +1,43 @@
 var r=100, g=100, b=100;
 
+var R=125, G=125, B=125;
+
 $(document).ready( function(){
-	alert('Bonjour! la couleur de la journée est Bleu ');
+	alert('Bonjour! la couleur du de la semaine: ? ');
 	
 	$('#rValue').on("change mousemove", function(){
 		r = $(this).val();		
 		$('#body').css({'background-color': 'rgb('+ r + ', ' +  g + ', ' + b +')'});
-		console.log(r);
+		
 	});
 
 	$('#gValue').on("change mousemove", function(){
 		g = $(this).val();		
 		$('#body').css({'background-color': 'rgb('+ r + ', ' +  g + ', ' + b +')'});
-		console.log(g);
+		
 	});
 
 	$('#bValue').on("change mousemove", function(){
 		b = $(this).val();		
 		$('#body').css({'background-color': 'rgb('+ r + ', ' +  g + ', ' + b +')'});
-		console.log(b);
+		
+	});
+
+	$('#RValue').on("change mousemove", function(){
+		R=$(this).val();
+		$('#body').css({'background': "linear-gradient(to right, rgb(" + R + "," + G + "," + B+ "),rgb(" + r + "," + g + "," + b + ")"}); 
+		console.log(R);
+	});
+
+	$('#GValue').on("change mousemove", function(){
+		G=$(this).val();
+		$('#body').css({'background': "linear-gradient(to right, rgb(" + R + "," + G + "," + B+ "),rgb(" + r + "," + g + "," + b + ")"}); 
+		console.log(G);
+	});
+
+	$('#BValue').on("change mousemove", function(){
+		B=$(this).val();
+		$('#body').css({'background': "linear-gradient(to right, rgb(" + R + "," + G + "," + B+ "),rgb(" + r + "," + g + "," + b + ")"}); 
+		console.log(B);
 	});
 })
